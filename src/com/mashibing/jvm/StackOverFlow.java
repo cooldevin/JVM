f763c4c0720945f74ed3266a5701d77d97ffc3f8
bjmashibing <alanpppbox@gmail.com>
2019-10-27 09:20:03 +0800

    init commit

--- src/com/mashibing/jvm/StackOverFlow.java
+++ src/com/mashibing/jvm/StackOverFlow.java
@@ -8,4 +8,5 @@ public class StackOverFlow {
     static void m() {
         m();
     }
+
 }
--- src/com/mashibing/jvm/bytecode/T0103_ByteCode03.java
+++
@@ -1,6 +0,0 @@
-package com.mashibing.jvm.bytecode;
-
-public class T0103_ByteCode03 {
-    int i = 0;
-    String s = "Hello ByteCode!";
-}
